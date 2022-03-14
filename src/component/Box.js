e64aefa7a8a0058b4d041b03b6e8b1bcaa5fbb0c
smackzack <ashishone9781@gmail.com>
2022-03-14 11:09:30 +0530

    addded the alt tags to img

--- src/component/Box.js
+++ src/component/Box.js
@@ -24,12 +24,12 @@ export default function BoxSx() {
             <Stack direction="row" spacing={2}>
                 <Tooltip title="Gmail" arrow>
                     <Button variant="text" href="https://www.addtoany.com/add_to/google_gmail?linkurl=https%3A%2F%2Fchrome.google.com%2Fwebstore%2Fdetail%2Fmeavana%2Fkpfiddidcfiieigdcgjhnkhmmbkipjcd%3Fhl%3Den-US%20Hey%20check%20this%20cool%20exetension%20&amp;linkname=MeaVana%20Extension" target="_blank" className='shareBtn' >
-                        <img src="https://img.icons8.com/color/48/000000/gmail-new.png" height='50px' width='50px' />
+                        <img src="https://img.icons8.com/color/48/000000/gmail-new.png" height='50px' width='50px' alt='gmail'  />
                     </Button>
                 </Tooltip>
                 <Tooltip title="Mail" arrow>
                     <Button variant="text" href="https://www.addtoany.com/add_to/email?linkurl=https%3A%2F%2Fchrome.google.com%2Fwebstore%2Fdetail%2Fmeavana%2Fkpfiddidcfiieigdcgjhnkhmmbkipjcd%3Fhl%3Den-US%20Hey%20check%20this%20cool%20exetension%20&amp;linkname=MeaVana%20Extension" target="_blank" className='shareBtn'>
-                        <img src="https://img.icons8.com/external-kiranshastry-solid-kiranshastry/64/000000/external-email-advertising-kiranshastry-solid-kiranshastry-1.png" height='60px' width='50px' />
+                        <img src="https://img.icons8.com/external-kiranshastry-solid-kiranshastry/64/000000/external-email-advertising-kiranshastry-solid-kiranshastry-1.png" height='60px' width='50px' alt='mail' />
                     </Button>
                 </Tooltip>
             </Stack>
